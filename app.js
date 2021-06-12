0d0e8891472f360d347ed221cb68056e4f6a70c7
Marke <mark@Mark2-MacBook-Air.local>
2021-06-12 23:50:49 +0100

    updates

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ var routes = require('./routes/index');
 
 var app = express();
 
+app.use(express.static('public'))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 // Uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname,'public','images','favicon.ico'));
